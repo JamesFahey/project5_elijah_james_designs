d7251642ed9be78e0be5cecadf5485f56a52e213
JamesFahey <james.fahey@hotmail.co.uk>
2022-05-27 21:59:02 +0000

    added stripe to checkout

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51L4AfPAbSspYVj6C6Sy23KQmKSfwYXPcfo95HCXDZkHjUFkyj4sxgYFbgobY4r2iKTZNE1KNHgqM0e9OuRvcDLwC00TFsvAJ9n',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
