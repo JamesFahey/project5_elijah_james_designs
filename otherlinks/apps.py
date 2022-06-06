9a8c6be9a0b4f31c21c82a387574115f23f04a96
JamesFahey <james.fahey@hotmail.co.uk>
2022-06-06 17:39:21 +0000

    style contact us and add views and url

---
+++ otherlinks/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OtherlinksConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'otherlinks'
---
+++ otherlinks/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+from django.contrib import messages
+
+def contact_us(request):
+    """View to return contact us page"""
+    # Help from Scottish Coder YouTube Tutorial - link in README
+    if request.method == 'POST':
+        # Send contact form to Gmail Account
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        message_data = {
+            'name': name,
+            'email': email,
+            'subject': subject,
+            'message': message,
+        }
+        message = '''
+        From: {}
+        New message: {}
+        '''.format(message_data['email'], message_data['message'])
+
+        send_mail(
+            message_data['subject'], message, '', ['jcfahey007@gmail.com'])
+
+        messages.info(request, (
+            f'Your message has been sent, we will contact you \
+                via { email } as soon as possible.'))
+        return render(request, 'home/index.html')
+
+    return render(request, 'contact/contact_us.html')
